e2568304842bcb4d8031c649abe526cb01461114
Dasha Finch <dasha@sitdap.com>
2010-10-14 14:38:57 +0000

    Image html extensions updated. Ability to specify default image added. Correct closing of image tag.

--- Source/Zeus.Templates/Mvc/Html/ImageExtensions.cs
+++ Source/Zeus.Templates/Mvc/Html/ImageExtensions.cs
@@ -6,57 +6,91 @@ namespace Zeus.Templates.Mvc.Html
 {
 	public static class ImageExtensions
 	{
-		public static string ImageUrl(this HtmlHelper helper, Image image, int width, int height, bool fill)
+        /// <summary>
+        /// Image methods
+        /// </summary>
+
+        public static string Image(this HtmlHelper helper, Image image)
+        {
+            return Image(helper, image, 0, 0);
+        }
+
+        public static string Image(this HtmlHelper helper, Image image, int width, int height)
+        {
+            return Image(helper, image, width, height, true);
+        }
+
+        public static string Image(this HtmlHelper helper, Image image, int width, int height, bool fill)
+        {
+            return Image(helper, image, width, height, fill, string.Empty);
+        }
+
+        public static string Image(this HtmlHelper helper, Image image, int width, int height, bool fill, string defaultImage)
+        {
+            string url = ImageUrl(helper, image, width, height, fill, defaultImage);
+            if (string.IsNullOrEmpty(url))
+                return string.Empty;
+
+            return ImageTag(helper, image, url);
+        }
+
+        /// <summary>
+        /// Image URL methods
+        /// </summary>
+        
+        public static string ImageUrl(this HtmlHelper helper, Image image)
+        {
+            return ImageUrl(helper, image, 0, 0);
+        }
+
+        public static string ImageUrl(this HtmlHelper helper, Image image, int width, int height)
 		{
-			if (image == null)
-				return string.Empty;
-			return image.GetUrl(width, height, fill);
+            return ImageUrl(helper, image, width, height, true);
 		}
 
-		public static string ImageUrl(this HtmlHelper helper, Image image, int width, int height)
-		{
-			if (image == null)
-				return string.Empty;
-			return image.GetUrl(width, height);
-		}
+        public static string ImageUrl(this HtmlHelper helper, Image image, int width, int height, bool fill)
+        {
+            return ImageUrl(helper, image, width, height, fill, string.Empty);
+        }
 
-		public static string ImageUrl(this HtmlHelper helper, Image image)
-		{
-			if (image == null)
-				return string.Empty;
-			return new DynamicImageBuilder()
-				.WithLayer(LayerBuilder.Image.SourceImage(image))
-				.Url;
-		}
+        public static string ImageUrl(this HtmlHelper helper, Image image, int width, int height, bool fill, string defaultImage)
+        {
+            string result = defaultImage;
 
-		public static string Image(this HtmlHelper helper, Image image, int width, int height, bool fill)
-		{
-			if (image == null)
-				return string.Empty;
-			return ImageTag(helper, image, ImageUrl(helper, image, width, height, fill));
-		}
+            // only generate url if image exists
+            if (image != null)
+            {
+                // special code for image without resizing
+                if (width == 0 && height == 0)
+                {
+                    result = new DynamicImageBuilder()
+                        .WithLayer(LayerBuilder.Image.SourceImage(image))
+                        .Url;
+                }
 
-		public static string Image(this HtmlHelper helper, Image image, int width, int height)
-		{
-			if (image == null)
-				return string.Empty;
-			return ImageTag(helper, image, ImageUrl(helper, image, width, height));
-		}
+                // generate resized image url
+                else
+                {
+                    result = image.GetUrl(width, height, fill);
+                }
+            }
 
-		public static string Image(this HtmlHelper helper, Image image)
-		{
-			if (image == null)
-				return string.Empty;
-			return ImageTag(helper, image, ImageUrl(helper, image));
-		}
+            return result;
+        }
+
+        /// <summary>
+        /// Image Tag methods
+        /// </summary>
 
 		public static string ImageTag(this HtmlHelper helper, Image image, string imageUrl)
 		{
-			TagBuilder imageTag = new TagBuilder("img");
-			imageTag.MergeAttribute("src", imageUrl, true);
-			imageTag.MergeAttribute("alt", image.Caption, true);
-			imageTag.MergeAttribute("border", "0", true);
-			return imageTag.ToString();
-		}
+            TagBuilder imageTag = new TagBuilder("img");
+
+            imageTag.MergeAttribute("src", imageUrl, true);
+            imageTag.MergeAttribute("alt", image != null ? image.Caption : string.Empty, true);
+            imageTag.MergeAttribute("border", "0", true);
+
+            return imageTag.ToString(TagRenderMode.SelfClosing);
+        }
 	}
 }
