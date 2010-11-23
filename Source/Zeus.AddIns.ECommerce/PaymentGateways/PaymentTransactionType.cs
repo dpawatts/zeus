bd84af44567f920a6b09b4dc23e41b388108d1c6
Dasha Finch <dasha@sitdap.com>
2010-11-23 09:13:32 +0000

    Missing files committed

---
+++ Source/Zeus.AddIns.ECommerce/PaymentGateways/PaymentTransactionType.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zeus.AddIns.ECommerce.PaymentGateways
+{
+    /// <summary>
+    /// Payment transaction types
+    /// </summary>
+    public enum PaymentTransactionType
+    {
+        /// <summary>
+        /// Undefined
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Payment
+        /// </summary>
+        Payment,
+
+        /// <summary>
+        /// Deferred
+        /// </summary>
+        Deferred,
+
+        /// <summary>
+        /// Repeat
+        /// </summary>
+        Repeat,
+
+        /// <summary>
+        /// Authenticate
+        /// </summary>
+        Authenticate,
+
+        /// <summary>
+        /// Authorise
+        /// </summary>
+        Authorise,
+
+        /// <summary>
+        /// Refund
+        /// </summary>
+        Refund,
+
+        /// <summary>
+        /// Void
+        /// </summary>
+        Void,
+
+        /// <summary>
+        /// Release
+        /// </summary>
+        Release,
+
+        /// <summary>
+        /// Abort
+        /// </summary>
+        Abort,
+
+        /// <summary>
+        /// Cancel
+        /// </summary>
+        Cancel
+    }
+}
