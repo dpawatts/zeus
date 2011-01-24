8e2e04115d81f15f3f17c702604fa66f117b3bed
Tim Jones <github@roastedamoeba.com>
2011-01-24 10:52:06 +0000

    Altered how Ninject modules are loaded. Zeus modules are loaded first, followed by non-Zeus modules. This allowed client projects to override (i.e. Rebind) service implementations.

--- Source/Zeus.BaseLibrary/DependencyInjection/DependencyInjectionManager.cs
+++ Source/Zeus.BaseLibrary/DependencyInjection/DependencyInjectionManager.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
 using Ninject;
 
 namespace Zeus.BaseLibrary.DependencyInjection
@@ -14,12 +18,70 @@ namespace Zeus.BaseLibrary.DependencyInjection
 			_kernel = new StandardKernel();
 			try
 			{
-				_kernel.Load("*.dll");
+				// Get all DLLS in bin folder.
+				IEnumerable<string> files = Directory.GetFiles(Path.GetDirectoryName(HttpRuntime.BinDirectory), "*.dll");
+
+				// Load modules in Zeus DLLs first.
+				_kernel.Load(FindAssemblies(files.Where(s => Path.GetFileName(s).StartsWith("Zeus."))));
+
+				// Then load non-Zeus DLLs - this gives projects a chance to override Zeus modules.
+				// Actually we just load all DLLs, because DLLs that have already been loaded
+				// won't get loaded again.
+				_kernel.Load(FindAssemblies(files.Where(s => !Path.GetFileName(s).StartsWith("Zeus."))));
 			}
-			catch (TypeLoadException ex)
+			catch (TypeLoadException)
 			{
 				
 			}
+		}
+
+		private static IEnumerable<Assembly> FindAssemblies(IEnumerable<string> filenames)
+		{
+			return FindAssemblyNames(filenames, a => true).Select(an => Assembly.Load(an));
+		}
+
+		private static IEnumerable<AssemblyName> FindAssemblyNames(IEnumerable<string> filenames,
+																																Predicate<Assembly> filter)
+		{
+			AppDomain temporaryDomain = CreateTemporaryAppDomain();
+
+			foreach (string file in filenames)
+			{
+				Assembly assembly;
+
+				if (File.Exists(file))
+				{
+					try
+					{
+						var name = new AssemblyName { CodeBase = file };
+						assembly = temporaryDomain.Load(name);
+					}
+					catch (BadImageFormatException)
+					{
+						// Ignore native assemblies
+						continue;
+					}
+				}
+				else
+				{
+					assembly = temporaryDomain.Load(file);
+				}
+
+				if (filter(assembly))
+				{
+					yield return assembly.GetName();
+				}
+			}
+
+			AppDomain.Unload(temporaryDomain);
+		}
+
+		private static AppDomain CreateTemporaryAppDomain()
+		{
+			return AppDomain.CreateDomain(
+					"AssemblyScanner",
+					AppDomain.CurrentDomain.Evidence,
+					AppDomain.CurrentDomain.SetupInformation);
 		}
 
 		public void Initialize()
