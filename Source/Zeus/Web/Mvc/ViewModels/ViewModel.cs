3425757dcf7bff094bdeb878421b3a31253f4ab8
dpawatts <dave@sitdap.com>
2011-03-30 13:37:24 +0100

    Bool added so you can tell if Change Signal was fired

--- Source/Zeus/Web/Mvc/ViewModels/ViewModel.cs
+++ Source/Zeus/Web/Mvc/ViewModels/ViewModel.cs
@@ -20,14 +20,17 @@ namespace Zeus.Web.Mvc.ViewModels
 			CurrentItem = currentItem;
 
             //fire changed signal if needed
+            ChangeSignalFired = false;
             var SessionVal = System.Web.HttpContext.Current.Application["zeusChange_" + currentItem.ID];
             if ((SessionVal == null) || (SessionVal != null && (System.DateTime)SessionVal != currentItem.Updated))
             {
                 _allDataSignal.FireChanged();
+                ChangeSignalFired = true;
                 System.Web.HttpContext.Current.Application["zeusChange_" + currentItem.ID] = currentItem.Updated;
             }
 		}
 
+        public bool ChangeSignalFired { get; set; }
         public static CacheSignal _allDataSignal = new CacheSignal();
 
         public ICacheSignal GetSignalForContentID
