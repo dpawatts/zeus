c08fb0fe58c8ed69cd582c28dc8d06eaaf791ca5
dpawatts <dave@sitdap.com>
2011-04-01 10:10:54 +0100

    Fix to TextEditorAttributeBase.cs - allow for initial value to be null (broken by .ToString() allowing comparison with integers etc
    Note : need to test dates to see if this works

--- Source/Zeus/Design/Editors/TextEditorAttributeBase.cs
+++ Source/Zeus/Design/Editors/TextEditorAttributeBase.cs
@@ -60,7 +60,7 @@ namespace Zeus.Design.Editors
 		{
 			ITextControl tb = editor as ITextControl;
 			string value = (tb.Text == DefaultValue) ? null : tb.Text;
-            if (!AreEqual(value, item[Name].ToString()))
+            if (!AreEqual(value, (item[Name] == null ? null : item[Name].ToString())))
 			{
 				item[Name] = value;
 				return true;
