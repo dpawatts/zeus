9795d5c27f65baa9c4bc8dc8b8f35c570d038c09
dpawatts <dave@sitdap.com>
2011-04-01 17:22:08 +0100

    Missing File for prev checkin

---
+++ Examples/MinimalMvcExample/Enums/Vegetables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace Zeus.Examples.MinimalMvcExample.Enums
+{
+    public enum Vegetable
+    {
+        [Description("Potato")]
+        Potato,
+
+        [Description("Runner Beans")]
+        RunnerBeans,
+
+        [Description("Red Onions")]
+        RedOnions
+    }
+}
