2590da8370254d35667a033405c62e91c1c0623c
dpawatts <dave@sitdap.com>
2012-10-16 01:01:21 +0100

    NHibernate Updated to use new version 3.3.1.4000

--- Source/Zeus.Persistence.NH/Linq/ZeusExtensions.cs
+++ Source/Zeus.Persistence.NH/Linq/ZeusExtensions.cs
@@ -14,10 +14,13 @@ namespace Zeus.Persistence.NH.Linq
 		/// <typeparam name="T">An NHibernate entity type.</typeparam>
 		/// <param name="session">An initialized <see cref="T:NHibernate.ISession"/> object.</param>
 		/// <returns>An <see cref="T:NHibernate.Linq.NHibernateQueryProvider"/> used to evaluate an expression tree.</returns>
-		public static INHibernateQueryable<T> Linq<T>(this ISession session)
+		public static System.Linq.IQueryable<T> Linq<T>(this ISession session)
 		{
+            /*
 			QueryOptions options = new QueryOptions();
 			return new Query<T>(new ZeusQueryProvider(session, options), options);
+             */
+            return session.Query<T>();
 		}
 	}
 }
